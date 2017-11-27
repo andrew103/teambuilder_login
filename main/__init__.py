21a48bca0c089742e9b4cfa150ded1ef8c1ef6a3
Andrew Euredjian <andrew_103@rocketmail.com>
2017-11-27 15:51:11 -0500

    Populate models.py and __init__.py

---
+++ main/__init__.py
@@ -0,0 +1,112 @@
+from flask import Flask, jsonify, request, g, make_response
+from flask import url_for, redirect, flash, render_template
+
+from flask import session as login_session
+
+from .models import Base, User
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from functools import wraps
+
+import random, string
+
+import flask_login
+from flask_login import LoginManager, login_user
+
+#===================== INIT CODE ============================
+
+engine = create_engine('sqlite:///user.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+app = Flask(__name__)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+app.secret_key = "userloginapplication"
+app.debug = True
+
+# ================= BEGIN LOGIN REQUIREMENT CODE ==============
+
+@login_manager.user_loader
+def load_user(user_id):
+    '''
+    Takes a unicode format user id and uses it to retrieve the respective user
+    object to be used by the login_manager
+    '''
+    user = session.query(User).filter_by(id=int(user_id)).first()
+    return user
+
+# ================== END LOGIN REQUIREMENT CODE ===============
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
+
+    login_session['state'] = state
+
+    if request.method == 'POST':
+        email = request.form['emailinput']
+        password = request.form['passinput']
+        try:
+            user = session.query(User).filter_by(email=email).first()
+            if user.verify_password(password):
+                if request.form['remember_me']:
+                    login_user(user, force=True, remember=True)
+                else:
+                    login_user(user, force=True)
+                flash("You have logged in successfully " + user.name)
+                user.is_authenticated = True
+                return redirect(url_for('showCatalog'))
+            else:
+                flash("You entered an incorrect password. Please try again")
+                return redirect(url_for('login'))
+        except:
+            flash("User does not exist. Please create an account")
+            return redirect(url_for('signup'))
+    else:
+        return render_template('login.html', STATE=state)
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    if request.method == 'POST':
+        flask_login.logout_user()
+        flash("Logout Successful")
+        return redirect(url_for('home'))
+    else:
+        return render_template('logout.html')
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        user = request.form['nameinput']
+        email = request.form['emailinput']
+        password = request.form['passinput']
+        newUser = User(name=user, email=email)
+        newUser.hash_password(password)
+
+        session.add(newUser)
+        session.commit()
+
+        login_user(newUser, force=True)
+        newUser.is_authenticated=True
+
+        flash("Welcome "+user+". You have successfully signed up")
+        return redirect(url_for('home'))
+    else:
+        return render_template('signup.html')
+
+
+@app.route('/')
+@app.route('/index')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
---
+++ main/models.py
@@ -0,0 +1,40 @@
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, ForeignKeyConstraint
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+
+from passlib.apps import custom_app_context as pwd_context
+import random, string
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(200), nullable=False)
+    email = Column(String(200))
+    password_hash = Column(String(300))
+    is_authenticated = Column(Boolean)
+    is_active = Column(Boolean)
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+    def get_id(self):
+        return str(self.id)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email,
+            'hash': self.password_hash,
+        }
+
+engine = create_engine('sqlite:///user.db')
+Base.metadata.create_all(engine)
